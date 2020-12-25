11fbc5908e4c06cc3829508d112131db8a887246
badrul2018 <badrulhaque2018@gmail.com>
2020-12-25 16:27:06 +0530

    Create_StudModel

--- prct_model_form01/testapp/admin.py
+++ prct_model_form01/testapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from testapp.models import StudModel
 
 # Register your models here.
+class StudAdmin(admin.ModelAdmin):
+    list_display=['name','roll','email','gender','mark']
+
+admin.site.register(StudModel,StudAdmin)    
--- prct_model_form01/testapp/models.py
+++ prct_model_form01/testapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class StudModel(models.Model):
+    name=models.CharField(max_length=30)
+    roll=models.CharField(max_length=5)
+    email=models.EmailField()
+    gender=models.CharField(max_length=6)
+    mark=models.FloatField()
+    def __str__(self):
+        return 'id'+self.roll
+
